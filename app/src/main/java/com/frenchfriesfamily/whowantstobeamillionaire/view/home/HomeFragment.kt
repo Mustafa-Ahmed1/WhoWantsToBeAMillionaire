85047ade10bd270cbba3c4f3ec4f2e0f2a54741c
Mohammed Najm <mohammed.najm.422@gmail.com>
2022-08-31 01:09:40 +0300

    implement home screen

--- app/src/main/java/com/frenchfriesfamily/whowantstobeamillionaire/view/home/HomeFragment.kt
+++ app/src/main/java/com/frenchfriesfamily/whowantstobeamillionaire/view/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import androidx.navigation.Navigation
 import com.frenchfriesfamily.whowantstobeamillionaire.R
 import com.frenchfriesfamily.whowantstobeamillionaire.databinding.FragmentHomeBinding
 import com.frenchfriesfamily.whowantstobeamillionaire.view.base.BaseFragment
+import kotlin.system.exitProcess
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home) {
 
@@ -11,13 +12,25 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.f
 
     override fun setUp() {
         navToQuestionFragment()
+        navToAboutFragment()
+        exitApp()
     }
 
     private fun navToQuestionFragment() {
-        binding.startBtn.setOnClickListener { view ->
-            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_questionFragment)
+        binding.buttonStart.setOnClickListener { view ->
+            Navigation.findNavController(view)
+                .navigate(R.id.action_homeFragment_to_questionFragment)
         }
+    }
 
+    private fun navToAboutFragment() {
+        binding.buttonAbout.setOnClickListener { view ->
+            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_aboutFragment)
+        }
+    }
+
+    private fun exitApp() {
+        binding.buttonExit.setOnClickListener { exitProcess(-1) }
     }
 
 }
