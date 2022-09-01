3af7b0202b3f555be9e817cefc402e4dda6013c6
Mohammed Najm <mohammed.najm.422@gmail.com>
2022-09-02 01:17:50 +0300

    handle sounds

---
+++ app/src/main/java/com/frenchfriesfamily/whowantstobeamillionaire/utils/Audio.kt
@@ -0,0 +1,25 @@
+package com.frenchfriesfamily.whowantstobeamillionaire.utils
+
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
+
+object Audio {
+    var muteState = 100
+    fun runAudio(mediaPlayer : MediaPlayer){
+        mediaPlayer.start()
+    }
+    fun pauseAudio(mediaPlayer : MediaPlayer){
+        mediaPlayer.pause()
+    }
+    fun muteAudio(context: Context){
+        var mAudioManager = context.getSystemService(AudioManager::class.java)
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0)
+        muteState = 0
+    }
+    fun unmuteAudio(context: Context){
+        var mAudioManager = context.getSystemService(AudioManager::class.java)
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 100, 0)
+        muteState = 100
+    }
+}
