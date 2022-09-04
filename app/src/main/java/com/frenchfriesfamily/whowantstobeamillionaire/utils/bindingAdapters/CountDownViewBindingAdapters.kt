fc48bf90d3799f032da521f6dce351c84724d477
alkomity <197023.it.eng@student.aden-univ.net>
2022-09-04 15:33:14 +0300

    add enum class for time color

--- app/src/main/java/com/frenchfriesfamily/whowantstobeamillionaire/utils/bindingAdapters/CountDownViewBindingAdapters.kt
+++ app/src/main/java/com/frenchfriesfamily/whowantstobeamillionaire/utils/bindingAdapters/CountDownViewBindingAdapters.kt
@@ -4,15 +4,16 @@ import androidx.databinding.BindingAdapter
 import com.androchef.happytimer.countdowntimer.CircularCountDownView
 import com.frenchfriesfamily.whowantstobeamillionaire.R
 import com.frenchfriesfamily.whowantstobeamillionaire.utils.Constants
+import com.frenchfriesfamily.whowantstobeamillionaire.utils.TimeDuration
 import com.frenchfriesfamily.whowantstobeamillionaire.utils.extensions.getColor
 
 @BindingAdapter(value = ["app:colorStateWithTimer"])
 fun changeCircularTimerColorDependingOnTime(view: CircularCountDownView, remainingSeconds: Int?) {
     when (remainingSeconds) {
-        in Constants.TimeDurations.ZERO..Constants.TimeDurations.FIRST_THIRD_DURATION -> {
+        in TimeDuration.FIRST_DURATION.duration -> {
             view.strokeColorForeground = view.getColor(R.color.red)
         }
-        in (Constants.TimeDurations.FIRST_THIRD_DURATION + 1)..Constants.TimeDurations.SECOND_THIRD_DURATION -> {
+        in TimeDuration.SECOND_DURATION.duration -> {
             view.strokeColorForeground = view.getColor(R.color.yellow)
         }
         else -> {
--- app/src/main/java/com/frenchfriesfamily/whowantstobeamillionaire/utils/bindingAdapters/TextViewBindingAdapters.kt
+++ app/src/main/java/com/frenchfriesfamily/whowantstobeamillionaire/utils/bindingAdapters/TextViewBindingAdapters.kt
@@ -4,16 +4,17 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.frenchfriesfamily.whowantstobeamillionaire.R
 import com.frenchfriesfamily.whowantstobeamillionaire.utils.Constants
+import com.frenchfriesfamily.whowantstobeamillionaire.utils.TimeDuration
 import com.frenchfriesfamily.whowantstobeamillionaire.utils.extensions.getColor
 import com.frenchfriesfamily.whowantstobeamillionaire.utils.extensions.replacePunctuationTextsWithSymbols
 
 @BindingAdapter(value = ["app:colorStateWithTimer"])
 fun changeTextColorDependingOnTime(view: TextView, remainingSeconds: Int?) {
     when (remainingSeconds) {
-        in Constants.TimeDurations.ZERO..Constants.TimeDurations.FIRST_THIRD_DURATION -> {
+        in TimeDuration.FIRST_DURATION.duration -> {
             view.setTextColor(view.getColor(R.color.red))
         }
-        in (Constants.TimeDurations.FIRST_THIRD_DURATION + 1)..Constants.TimeDurations.SECOND_THIRD_DURATION -> {
+        in TimeDuration.SECOND_DURATION.duration -> {
             view.setTextColor(view.getColor(R.color.yellow))
         }
         else -> {
