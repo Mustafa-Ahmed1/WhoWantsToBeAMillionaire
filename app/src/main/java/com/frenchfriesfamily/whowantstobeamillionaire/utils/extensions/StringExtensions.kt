6a7aabf8cc71b2246d6ab96ca542abb68b7e662f
Mostafa Mohamed <tofymofy.55@gmail.com>
2022-09-07 00:06:26 +0300

    move bar chart properties to binding adapters

--- app/src/main/java/com/frenchfriesfamily/whowantstobeamillionaire/utils/extensions/StringExtensions.kt
+++ app/src/main/java/com/frenchfriesfamily/whowantstobeamillionaire/utils/extensions/StringExtensions.kt
@@ -1,5 +1,6 @@
 package com.frenchfriesfamily.whowantstobeamillionaire.utils.extensions
 
+import com.github.mikephil.charting.data.BarEntry
 import org.jsoup.Jsoup
 import kotlin.random.Random
 
@@ -21,4 +22,26 @@ fun String.getProbableAnswer(answers: List<String?>?): Char {
     friendAnswer = (probabilities.indexOf(maximumValue) + 65).toChar()
 
     return friendAnswer
+
+}
+
+fun List<String?>?.getProbableAudienceAnswers(correctAnswer: String?): ArrayList<BarEntry> {
+    val barEntries = arrayListOf<BarEntry>()
+    val probabilities = mutableListOf<Float>()
+
+    this?.forEach { answer ->
+        if (answer == correctAnswer) {
+            probabilities.add(Random.nextFloat() + 1f)
+        } else {
+            probabilities.add(Random.nextFloat())
+        }
+    }
+
+    val sum = probabilities.sum()
+    probabilities.forEachIndexed { index, probability ->
+        val barEntry = BarEntry(probability / sum * 100, index)
+        barEntries.add(barEntry)
+    }
+
+    return barEntries
 }
